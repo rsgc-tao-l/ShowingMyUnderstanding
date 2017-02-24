9bf41668a21abe9563a665e30eb2dc0533818018
Student <student@mbp0435luketao>
2017-02-23 22:10:15 -0500

    example for loop finished. this is a picture of many guns shooting bullets at times different from each other.

--- ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Iteration - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Iteration - Create an Example.xcplaygroundpage/Contents.swift
@@ -18,6 +18,17 @@ import PlaygroundSupport
 
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
+//lots of guns
+canvas.drawShapesWithBorders = false
+canvas.drawShapesWithFill = true
+for x in stride(from: 20, through: 300, by: 58){
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: x, width: 100, height: 20)
+canvas.drawRectangle(bottomLeftX: 4, bottomLeftY: x+20, width: 5, height: 10)
+//constant bullets at random places.
+let gun = random(from: 120, toButNotIncluding: 300)
+    canvas.drawRectangle(bottomLeftX: gun, bottomLeftY: x, width: 13, height: 10)
+}
+
 
 // Replace this comment and add your code below...
 
